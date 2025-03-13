4edc4594366c4025450573ce2dc42792851f89fe
yudin5 <yudin5@yandex.ru>
2025-03-13 17:52:27 +0300

    initial commit

---
+++ src/main/java/com/example/javaprofinal/JavaProFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaprofinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProFinalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProFinalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaprofinal/JavaProFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprofinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
